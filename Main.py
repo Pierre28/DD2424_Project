835f57f1d0ec6fa53456543ffb1b262dbff64437
benoitcharmettant <benoit.charmettant@student.ecp.fr>
2018-05-16 16:06:49 +0200

    Comments main ( acctept .gz format)

--- Main.py
+++ Main.py
@@ -3,11 +3,14 @@ from mnist import MNIST
 import numpy as np
 
 
-def main():
+def main(gz = True):
+    """
+    @param: gz boolean, equals True if data is .gz (compressed) format. False otherwise
+    """
     path_to_dataset = os.path.join('Datasets', 'MNIST')
 
     mndata = MNIST(path_to_dataset)
-    mndata.gz = True # Données en format .gz dans le dossier Datasets\MNIST
+    mndata.gz = gz # Données en format .gz dans le dossier Datasets\MNIST
     images, _ = mndata.load_training()
     images = np.array(images)/255
     dcgan = DCGAN([28, 28, 1], first_block_depth=20, simple_model=True)
