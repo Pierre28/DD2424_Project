52c6e0fe892d5afb88bf57f837e5643d9f082317
Benoit Laurès <benoit.laures@student.ecp.fr>
2018-05-16 13:41:29 +0200

    recleaned repo

--- Main.py
+++ Main.py
@@ -7,8 +7,7 @@ def main():
     path_to_dataset = os.path.join('Datasets', 'MNIST')
 
     mndata = MNIST(path_to_dataset)
-    mndata.gz = True # Sous quel format sont vos données ? Moi ça marche quand je ne les décompresse pas mais je dois ajouter cette ligne
-    # pour etre sur d'etre clair j'ai mes données en format .gz dans le dossier Datasets\MNIST
+    mndata.gz = True # Données en format .gz dans le dossier Datasets\MNIST
     images, _ = mndata.load_training()
     images = np.array(images)/255
     dcgan = DCGAN([28, 28, 1], first_block_depth=20, simple_model=True)
